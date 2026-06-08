d3c32419bad4b9c09237d9579a9df194c2530579
Deepak2005-23 <deepaksathiyamoorthy1@gmail.com>
2026-06-08 22:44:37 +0530

    Function

---
+++ class 6 Function/concat.js
@@ -0,0 +1,4 @@
+function add(a,b,c=1){
+    console.log(a+b+c);
+}
+add("Deepak","Rathish")  //DeepakRathish1
---
+++ class 6 Function/one.js
@@ -0,0 +1,5 @@
+function add(a,b,c){
+    console.log(a+b+c);
+}
+add(10,20,30);  // 60
+add(1,2,);      // 10+20+undefined = Nan
